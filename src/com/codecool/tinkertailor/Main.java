31994e56027c4142c625304ecc7bab5a28c41e19
Mate Balogh <b.mate11@gmail.com>
2017-08-10 17:05:04 +0200

    algorithm was implemented with arraylist and linkedlist as well

--- src/com/codecool/tinkertailor/Main.java
+++ src/com/codecool/tinkertailor/Main.java
@@ -1,18 +1,20 @@
 package com.codecool.tinkertailor;
-
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-/*	    TinkerTailor tinkerTailor = new TinkerTailor(5, 3);
+	    TinkerTailor tinkerTailor = new TinkerTailor(5, 3);
 	    List outcome = tinkerTailor.execute();
-	    System.out.println("The outcome of the game is: " + outcome);*/
+	    System.out.println("The outcome of the game is: " + outcome);
 
-		TinkerTest testyke = new TinkerTest(5, 8);
-		List result = testyke.Tinker2nd();
-		System.out.println("The final result is: " + result + " Winner: " + result.get(result.size() - 1));
+		TinkerTest test1st = new TinkerTest(5, 3);
+		List result = test1st.Tinker2nd();
+		System.out.println("The result is: " + result + " Winner: " + result.get(result.size() - 1));
+
+		TinkerTest2nd test2nd = new TinkerTest2nd(5, 3);
+		List finalResult = test2nd.Tinker3rd();
+		System.out.println("The result is: " + finalResult);
 	}
 }
